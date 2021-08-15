01085283fcc508a331fd1839658d317273c77e33
Michael White <cclloyd@cclloyd.com>
2021-08-15 16:54:16 -0400

    Initial

---
+++ src/module/preloadTemplates.js
@@ -0,0 +1,7 @@
+export async function preloadTemplates() {
+  const templatePaths = [
+    // Add paths to "modules/pf1-kineticist-enhancements/templates"
+  ];
+
+  return loadTemplates(templatePaths);
+}
